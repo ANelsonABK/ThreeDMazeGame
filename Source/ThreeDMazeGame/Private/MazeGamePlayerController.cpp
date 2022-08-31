35980d708fc0cc7ec9d3f16413b66d91624157b2
ANelsonABK <Alexanderia.Nelson@activision.com>
2022-08-31 17:26:06 -0400

    Initial commit

---
+++ Source/ThreeDMazeGame/Private/MazeGamePlayerController.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MazeGamePlayerController.h"
+
+void AMazeGamePlayerController::BeginPlay()
+{
+}
+
+void AMazeGamePlayerController::ReceivedPlayer()
+{
+}
+
+void AMazeGamePlayerController::OnPossess(APawn* aPawn)
+{
+}
+
+void AMazeGamePlayerController::OnUnPossess()
+{
+}
+
+void AMazeGamePlayerController::SetupInputComponent()
+{
+}
+
+bool AMazeGamePlayerController::CanProcessRequest() const
+{
+	return false;
+}
+
+void AMazeGamePlayerController::RequestMoveForward(float AxisValue)
+{
+}
+
+void AMazeGamePlayerController::RequestMoveRight(float AxisValue)
+{
+}
+
+void AMazeGamePlayerController::RequestLookUp(float AxisValue)
+{
+}
+
+void AMazeGamePlayerController::RequestLookRight(float AxisValue)
+{
+}
+
+void AMazeGamePlayerController::RequestThrowObject(float AxisValue)
+{
+}
+
+void AMazeGamePlayerController::RequestPullorAimObject()
+{
+}
+
+void AMazeGamePlayerController::RequestStopPullorAimObject()
+{
+}
+
+void AMazeGamePlayerController::RequestJump()
+{
+}
+
+void AMazeGamePlayerController::RequestStopJump()
+{
+}
+
+void AMazeGamePlayerController::RequestCrouchStart()
+{
+}
+
+void AMazeGamePlayerController::RequestCrouchEnd()
+{
+}
+
+void AMazeGamePlayerController::RequestSprintStart()
+{
+}
+
+void AMazeGamePlayerController::RequestSprintEnd()
+{
+}
---
+++ Source/ThreeDMazeGame/ThreeDMazeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThreeDMazeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREEDMAZEGAME_API AThreeDMazeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
